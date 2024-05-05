e6aee344ee513364c07d1c4a85d935a468747147
rezwans1027 <rezwans1027@gmail.com>
2024-05-04 23:20:28 -0400

    added types for supabase and stripe

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { Database } from "@/types_db" 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import React from 'react'
+
+interface SupabaseProviderProps {
+    children: React.ReactNode;
+}
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({children}) => {
+    const [supabaseClient] = React.useState(() => 
+        createClientComponentClient<Database>()
+    );
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient} >
+        {children}
+    </SessionContextProvider>
+  )
+}
+
+export default SupabaseProvider
