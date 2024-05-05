e4276e97a935d0e2bce5646619f6f923c8758746
rezwans1027 <rezwans1027@gmail.com>
2024-05-04 20:47:41 -0400

    finished base layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,10 +10,20 @@ export default function Home() {
             Welcome back 
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem />
+            <ListItem image="/images/liked.png" name="Liked Songs" href="liked" />
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Newest songs
+          </h1>
+        </div>
+        <div>
+          List of Songs
+        </div>
+      </div>
     </div>
   );
 }
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
+import { FaPlay } from "react-icons/fa";
 
 interface ListItemProps {
   image: string;
@@ -17,8 +19,14 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
   };
 
   return (
-    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
-      <div className="relative min-h-[64px] min-w-[64px]"></div>
+    <button onClick={onClick} className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+      <div className="relative min-h-[64px] min-w-[64px]">
+        <Image src={image} alt={name} layout="fill" objectFit="cover" />
+      </div>
+      <p className="truncate py-5 font-medium">{name}</p>
+      <div className="absolute tansition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+        <FaPlay className="text-black" />
+      </div>
     </button>
   );
 };
